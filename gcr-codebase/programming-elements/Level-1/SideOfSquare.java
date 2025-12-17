78187305ddd30fb39fdee9a81897dc62ddae9cd7
harshita_dubey25 <harshi.d2507@gmail.com>
2025-12-17 14:40:18 +0530

    [Harshita] : add. added SideOfSquare.java file

---
+++ gcr-codebase/programming-elements/Level-1/SideOfSquare.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class SideOfSquare
+{
+   public static void main(String[] args)
+   {
+        Scanner input=new Scanner(System.in);
+        float perimeter=input.nextFloat();
+	float side=perimeter/4;
+	System.out.println("The length of the side is "+side+"  whose perimeter is "+perimeter);
+   } 
+}
