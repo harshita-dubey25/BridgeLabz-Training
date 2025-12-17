710624f296c6da68c0af4ec461db0c4ca1132736
harshita_dubey25 <harshi.d2507@gmail.com>
2025-12-17 14:38:45 +0530

    [Harshita] : add. added PCMPercentage.java file

---
+++ gcr-codebase/programming-elements/Level-1/PCMPercentage.java
@@ -0,0 +1,11 @@
+class PCMPercentage
+{
+   public static void main(String[] args)
+   {
+       int physics=94;
+       int maths=95;
+       int chemistry=96;
+       float average=(physics+maths+chemistry)/3;
+       System.out.println(average);
+   }
+}
