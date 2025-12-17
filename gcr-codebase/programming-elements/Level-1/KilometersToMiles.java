d36e771219f655820e46890743a2f956314be629
harshita_dubey25 <harshi.d2507@gmail.com>
2025-12-17 14:36:43 +0530

    [Harshita] : add. added KilometersToMiles.java file

---
+++ gcr-codebase/programming-elements/Level-1/KilometersToMiles.java
@@ -0,0 +1,9 @@
+class KilometersToMiles
+{
+    public static void main(String[] args)
+    {
+        float km=10.8f;
+	double miles=km*1.6;
+	System.out.println("The distance "+km+"km in miles is" +miles);
+    }
+}
