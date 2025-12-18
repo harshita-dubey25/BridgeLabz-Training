e0e1bcf3044cf805d59329f12a414141c2a304ba
harshita_dubey25 <harshi.d2507@gmail.com>
2025-12-18 23:35:11 +0530

    [Harshita] : add. added LeapYear2.java file

---
+++ gcr-codebase/control-flows/Level-3/LeapYear2.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class LeapYear2
+{
+public static void main(String[] args)
+{
+	Scanner input = new Scanner(System.in);
+	
+	int year = input.nextInt();
+	
+	if(year<=1582){
+	System.out.println("Not a valid year");
+	System.exit(0);
+	}
+	
+	if(year%400==0 || (year%4==0 && year%100!=0))
+	{
+	System.out.println("Leap Year");
+	}
+	else{
+	System.out.println("Not a Leap Year");
+	}
+}
+}
