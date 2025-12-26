87af5acc14504f292cf34231519ede2ca6825325
harshita_dubey25 <harshi.d2507@gmail.com>
2025-12-26 12:27:52 +0530

    [Harshita] : added java-opps-practice folder

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/classandobject/SampleClass.java
@@ -0,0 +1,11 @@
+package com.classandobject;
+
+public class SampleClass {
+
+	public static void main(String[] args) {
+	    System.out.println("Hii Guys!!!");
+	    System.out.println("Hello!!!!!");
+
+	}
+
+}
