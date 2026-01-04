819c2f8a6fdf4d623d1d07369c470c89c97aa783
harshita_dubey25 <harshi.d2507@gmail.com>
2026-01-04 21:05:51 +0530

    [Harshita] : added Task Scheduler using Linked List

--- dsa-practice/gcr-codebase/DataStructure/src/com/linkedlist/moviemanagement/Movie.java
+++ dsa-practice/gcr-codebase/DataStructure/src/com/linkedlist/moviemanagement/Movie.java
@@ -1,5 +1,21 @@
 package com.linkedlist.moviemanagement;
 
-public class Movie {
 
+public class Movie {
+	public String title;
+	public String director;
+	public int releaseYear;
+	public double rating;
+	Movie next;
+	Movie prev;
+	
+	public Movie(String title, String director, int releaseYear, double rating) {
+		this.title = title;
+		this.director = director;
+		this.releaseYear = releaseYear;
+		this.rating = rating;
+		this.next = null;
+		this.prev = null;
+	}
+	
 }
--- dsa-practice/gcr-codebase/DataStructure/src/com/linkedlist/moviemanagement/MovieMain.java
+++ dsa-practice/gcr-codebase/DataStructure/src/com/linkedlist/moviemanagement/MovieMain.java
@@ -1,5 +1,21 @@
 package com.linkedlist.moviemanagement;
 
-public class MovieMain {
 
+public class MovieMain {
+	public String title;
+	public String director;
+	public int releaseYear;
+	public double rating;
+	Movie next;
+	Movie prev;
+	
+	public MovieMain(String title, String director, int releaseYear, double rating) {
+		this.title = title;
+		this.director = director;
+		this.releaseYear = releaseYear;
+		this.rating = rating;
+		this.next = null;
+		this.prev = null;
+	}
+	
 }
