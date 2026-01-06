1d4ad92e9de7a4fbcea51d207994db92a2763906
harshita_dubey25 <harshi.d2507@gmail.com>
2026-01-06 14:42:39 +0530

    [Harshita] : modified Faculty.java

--- java-oops-practice/scenario-based-codebase/ScenarioBased/src/com/campusconnect/Faculty.java
+++ java-oops-practice/scenario-based-codebase/ScenarioBased/src/com/campusconnect/Faculty.java
@@ -1,13 +1,13 @@
 package com.campusconnect;
 
-public class Faculty extends Person {
+public class Faculty extends Person{
 	private String department;
 	Faculty(String name, String email, int id,String department){
 		super(name,email,id);
 		this.department = department;
 	}
 	
-	void displayDetails() {
+	void displayDetails(){
 		System.out.println("Faculty Name: "+name+"\nId: "+id+"\nDepartment: "+department);
 	}
 }
