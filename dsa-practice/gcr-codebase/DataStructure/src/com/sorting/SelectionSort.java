fddc43e49f7f27ad71d22c95fbcd3ea6238099f9
harshita_dubey25 <harshi.d2507@gmail.com>
2026-01-06 12:03:30 +0530

    [Harshita] : added Selection Sort in sorting

---
+++ dsa-practice/gcr-codebase/DataStructure/src/com/sorting/SelectionSort.java
@@ -0,0 +1,31 @@
+package com.sorting;
+
+public class SelectionSort {
+
+    public static void selectionSort(int[] scores) {
+        int n = scores.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+
+            for (int j = i + 1; j < n; j++) {
+                if (scores[j] < scores[minIndex])
+                    minIndex = j;
+            }
+
+            int temp = scores[minIndex];
+            scores[minIndex] = scores[i];
+            scores[i] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] scores = {78, 45, 90, 60, 85};
+
+        selectionSort(scores);
+
+        System.out.println("Sorted Exam Scores:");
+        for (int score : scores)
+            System.out.print(score + " ");
+    }
+}
