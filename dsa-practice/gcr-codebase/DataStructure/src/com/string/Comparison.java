7b2ef0a451331872d74571d9eb8ebe61f51ef2e2
harshita_dubey25 <harshi.d2507@gmail.com>
2026-01-08 12:10:32 +0530

    [Harshita] : added Challenge problem of String in dsa-practice

---
+++ dsa-practice/gcr-codebase/DataStructure/src/com/string/Comparison.java
@@ -0,0 +1,33 @@
+package com.string;
+
+public class Comparison {
+
+    public static void main(String[] args) {
+
+        int iterations = 1_000_000;
+
+        StringBuffer stringBuffer = new StringBuffer();
+        long startBuffer = System.nanoTime();
+
+        for (int i = 0; i < iterations; i++) {
+            stringBuffer.append("hello");
+        }
+
+        long endBuffer = System.nanoTime();
+        long bufferTime = endBuffer - startBuffer;
+
+        
+        StringBuilder stringBuilder = new StringBuilder();
+        long startBuilder = System.nanoTime();
+
+        for (int i = 0; i < iterations; i++) {
+            stringBuilder.append("hello");
+        }
+
+        long endBuilder = System.nanoTime();
+        long builderTime = endBuilder - startBuilder;
+
+        System.out.println("StringBuffer Time   : " + bufferTime + " ns");
+        System.out.println("StringBuilder Time : " + builderTime + " ns");
+    }
+}
