01154f52ef363f2075a3670243b3a8e57bdb4646
harshita_dubey25 <harshi.d2507@gmail.com>
2026-01-17 11:19:57 +0530

    [Harshita] : add Homenest Problem

---
+++ java-oops-practice/scenario-based-codebase/ScenarioBased/src/com/homenest/Thermostat.java
@@ -0,0 +1,13 @@
+package homenest;
+
+public class Thermostat extends Device{
+	public Thermostat(String deviceId) {
+        super(deviceId, 40.0);
+    }
+
+    @Override
+    public void reset() {
+        turnOff();
+        System.out.println("Thermostat reset: temperature normalized");
+    }
+}
