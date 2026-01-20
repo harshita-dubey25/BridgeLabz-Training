f08bdbfe805f9a2cf17cc344457449ba71cdc59c
harshita_dubey25 <harshi.d2507@gmail.com>
2026-01-20 10:12:36 +0530

    [Harshita] : add Reverse A Queue

---
+++ java-collections-practice/gcr-codebase/collections/src/collectionsproblems/QueueInterface/ReverseAQueue.java
@@ -0,0 +1,22 @@
+package collectionsproblems.QueueInterface;
+import java.util.*;
+
+public class ReverseAQueue {
+    static void reverseQueue(Queue<Integer> q) {
+        if (q.isEmpty()) return;
+
+        int front = q.remove();
+        reverseQueue(q);
+        q.add(front);
+    }
+
+    public static void main(String[] args) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(10);
+        q.add(20);
+        q.add(30);
+
+        reverseQueue(q);
+        System.out.println(q); 
+    }
+}
