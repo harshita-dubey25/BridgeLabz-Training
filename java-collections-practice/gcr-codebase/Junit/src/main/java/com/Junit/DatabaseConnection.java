226c57dee78b00f51a680eca1bb33bf3b0b6bed6
harshita_dubey25 <harshi.d2507@gmail.com>
2026-01-27 15:40:55 +0530

    [Harshita] : add Database Connection Testing

---
+++ java-collections-practice/gcr-codebase/Junit/src/main/java/com/Junit/DatabaseConnection.java
@@ -0,0 +1,17 @@
+package com.Junit;
+
+public class DatabaseConnection {
+    private boolean isConnected = false;
+
+    public void connect() {
+        isConnected = true;
+    }
+
+    public void disconnect() {
+        isConnected = false;
+    }
+
+    public boolean isConnected() {
+        return isConnected;
+    }
+}
