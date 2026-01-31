e005c7b3bb104593caa1073213ec1a2ccfbf563e
harshita_dubey25 <harshi.d2507@gmail.com>
2026-01-31 13:18:25 +0530

    [Harshita] : add Custom Annotation

---
+++ java-collections-practice/gcr-codebase/collections/src/annotation/CustomAnnotation/TaskManager.java
@@ -0,0 +1,9 @@
+package annotation.CustomAnnotation;
+
+public class TaskManager {
+    // Method annotated with TaskInfo
+    @TaskInfo(priority = "HIGH", assignedTo = "AP")
+    public void completeTask() {
+        System.out.println("Task is being completed.");
+    }
+}
