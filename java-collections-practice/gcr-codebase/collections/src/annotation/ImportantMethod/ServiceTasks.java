ba114cd4ea5de55d25eacabd8742a1900ce430ff
harshita_dubey25 <harshi.d2507@gmail.com>
2026-02-02 12:07:34 +0530

    [Harshita] : add Important method annotation

---
+++ java-collections-practice/gcr-codebase/collections/src/annotation/ImportantMethod/ServiceTasks.java
@@ -0,0 +1,16 @@
+package annotation.ImportantMethod;
+
+public class ServiceTasks {
+
+    // Important method with default level
+    @ImportantMethod
+    public void backupDatabase() {
+        System.out.println("Database backup completed.");
+    }
+
+    // Important method with custom level
+    @ImportantMethod(level = "MEDIUM")
+    public void generateReport() {
+        System.out.println("Report generated.");
+    }
+}
