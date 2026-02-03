56e7e11aa92b00942502d48e9fc0d22bca7c6c22
harshita_dubey25 <harshi.d2507@gmail.com>
2026-02-03 11:50:13 +0530

    [Harshita] : add logexecution

--- java-collections-practice/gcr-codebase/collections/src/annotation/loggingexecution/LogExecutionTime.java
+++ java-collections-practice/gcr-codebase/collections/src/annotation/loggingexecution/LogExecutionTime.java
@@ -1,5 +1,13 @@
 package annotation.loggingexecution;
 
-public interface LogExecutionTime {
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.annotation.ElementType;
 
+// Annotation to mark methods for execution time logging
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface LogExecutionTime {
+    
 }
